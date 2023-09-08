3e4904b1a07cd0cb4745146524834d6c8298067b
rohit07sprinklr <105945261+rohit07sprinklr@users.noreply.github.com>
2023-09-08 16:46:50 +0530

    git changes

--- src/components/Avatar/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-import './avatar.css';
-
-type AvatarProps = { avatarSrc: string; size: string };
-
-export default function Avatar({ avatarSrc, size }: AvatarProps) {
-  return <img src={avatarSrc} className={'avatar-container ' + size}></img>;
-}
